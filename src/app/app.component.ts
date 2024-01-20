9870b7326a5aaeb6aedf183dbd9a79f7492989a5
yuanfu <yuanfu@niu.edu.tw>
2024-01-20 15:29:53 +0800

    is work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,24 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import {  TranslateModule, TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ngTranTest';
+
+constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'zh-tw']);
+    translate.setDefaultLang('en');
+
+    const browserLang = translate.getBrowserLang();
+    translate.use('zh-tw');
+  }
+
 }
